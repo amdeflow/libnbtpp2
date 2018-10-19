045bec9e942e0be5fd237c6160de4faa4944265a
Rutger Broekhoff <rutgerbroekhoff3@gmail.com>
2018-10-19 09:26:12 +0200

    Add all_tags header

---
+++ include/nbtpp2/all_tags.hpp
@@ -0,0 +1,17 @@
+#ifndef NBTPP2_ALL_TAGS_HPP
+#define NBTPP2_ALL_TAGS_HPP
+
+#include "tags/tag_byte.hpp"
+#include "tags/tag_short.hpp"
+#include "tags/tag_int.hpp"
+#include "tags/tag_long.hpp"
+#include "tags/tag_float.hpp"
+#include "tags/tag_double.hpp"
+#include "tags/tag_byte_array.hpp"
+#include "tags/tag_string.hpp"
+#include "tags/tag_list.hpp"
+#include "tags/tag_compound.hpp"
+#include "tags/tag_int_array.hpp"
+#include "tags/tag_long_array.hpp"
+
+#endif //NBTPP2_ALL_TAGS_HPP
