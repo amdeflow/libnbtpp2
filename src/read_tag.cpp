ebb49e4fbcefcd335510ba5d503e660367287a49
Rutger Broekhoff <rutgerbroekhoff3@gmail.com>
2018-11-24 13:12:27 +0100

    Use all_tags.hpp instead of including all tags in read_tag.cpp

--- src/read_tag.cpp
+++ src/read_tag.cpp
@@ -1,17 +1,5 @@
 #include "nbtpp2/read_tag.hpp"
-
-#include "nbtpp2/tags/tag_byte.hpp"
-#include "nbtpp2/tags/tag_byte_array.hpp"
-#include "nbtpp2/tags/tag_short.hpp"
-#include "nbtpp2/tags/tag_int.hpp"
-#include "nbtpp2/tags/tag_long.hpp"
-#include "nbtpp2/tags/tag_float.hpp"
-#include "nbtpp2/tags/tag_double.hpp"
-#include "nbtpp2/tags/tag_list.hpp"
-#include "nbtpp2/tags/tag_int_array.hpp"
-#include "nbtpp2/tags/tag_long_array.hpp"
-#include "nbtpp2/tags/tag_string.hpp"
-#include "nbtpp2/tags/tag_compound.hpp"
+#include "nbtpp2/all_tags.hpp"
 
 namespace nbtpp2
 {
